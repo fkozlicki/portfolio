79edc7c71f9e88ca93f071a0c9645ddcd0b6bba3
Filip Kozlicki <filip.kozlickii@gmail.com>
2022-04-29 18:41:36 +0200

    image and description

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,4 +1,3 @@
-import image from "../images/about.jpg";
 import SectionHeader from "./SectionHeader";
 const About = () => {
 	return (
@@ -7,20 +6,22 @@ const About = () => {
 			id="about"
 		>
 			<SectionHeader title={"O mnie"} subtitle={"Kilka słów"} />
-			<div
-				className="md:max-w-screen-md xl:max-w-screen-lg mx-3 sm:mx-6 md:m-auto"
-				data-aos="zoom-in-up"
-				data-aos-delay="500"
-			>
-				<div className="max-w-md md:max-w-none m-auto grid grid-cols-1 md:grid-cols-2 gap-y-8 gap-x-8 text-center xl:text-start items-center">
-					<div className="p-4 bg-aliceblue dark:bg-navy-600 rounded-xl duration-300">
-						<img className="w-full h-auto rounded-xl" src={image} alt="about" />
-					</div>
-					<p className="text-[15px] md:text-base md:text-lg text-center md:text-left ">
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates,
-						perferendis numquam modi libero labore porro explicabo? Minima quos
-						voluptate expedita corporis reiciendis, cupiditate a iusto pariatur,
-						quidem, sit numquam? Magnam.
+			<div className="mx-3 sm:mx-6 md:m-auto max-w-screen-md md:max-w-screen-lg">
+				<div className="grid justify-items-center gap-y-10 gap-x-16 md:grid-cols-2 items-center">
+					<img
+						className="md:w-60 rounded-xl bg-aliceblue dark:bg-navy-600 duration-300"
+						src="/images/me.webp"
+						alt="about"
+						width={170}
+						height={300}
+					/>
+
+					<p className="text-[15px] md:text-base text-center md:text-left dark:font-extralight">
+						Cześć! Jestem Filip, mam 20 lat i jestem studentem pierwszego roku
+						informatyki na UMCS'ie. Front-endu uczę się samemu i aktualnie
+						jestem na etapie rozwijania umiejętności budowania aplikacji
+						webowych przy użyciu nowoczesnych narzędzi. Sprawdź i oceń co
+						potrafię ↓
 					</p>
 				</div>
 			</div>
