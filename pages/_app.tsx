8af282c02a1609619aa6b8b6889a2ee600a83fe8
Filip Koźlicki <filip.kozlickii@gmail.com>
2023-02-17 23:26:53 +0100

    Added LocomotiveScroll provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,30 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+import '../src/styled/globals.css';
+import { LocomotiveScrollProvider } from 'react-locomotive-scroll';
+import { useRef } from 'react';
+import { useRouter } from 'next/router';
+import ScrollTriggerProxy from '@/src/components/ScrollTriggerProxy';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	const containerRef = useRef<HTMLElement>(null);
+	const { asPath } = useRouter();
+
+	return (
+		<LocomotiveScrollProvider
+			options={{
+				smooth: true,
+				multiplier: 1,
+			}}
+			watch={[]}
+			location={asPath}
+			onLocationChange={(scroll: any) =>
+				scroll.scrollTo(0, { duration: 0, disableLerp: true })
+			}
+		>
+			<ScrollTriggerProxy />
+			<main data-scroll-container ref={containerRef}>
+				<Component {...pageProps} />
+			</main>
+		</LocomotiveScrollProvider>
+	);
 }
