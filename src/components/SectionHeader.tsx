1b3bb1be7cef0ef4ef8f16be86a4740da3fab66e
Filip Koźlicki <filip.kozlickii@gmail.com>
2023-02-28 17:30:04 +0100

    Updated SectionHeader component

--- src/components/SectionHeader.tsx
+++ src/components/SectionHeader.tsx
@@ -7,16 +7,15 @@ interface SectionHeaderProps {
 
 const SectionHeader = ({ children }: SectionHeaderProps) => {
 	return (
-		<StyledHeader>
+		<StyledHeader data-scroll>
 			{children.split('').map((letter, index) => (
-				<HeaderContent key={index} data-scroll>
-					<HeaderContent
-						data-scroll
-						data-scroll-speed="4"
-						data-scroll-delay={(0.04 + 0.01 * (index + 1)).toFixed(2)}
-					>
-						{letter}
-					</HeaderContent>
+				<HeaderContent
+					key={index}
+					data-scroll
+					data-scroll-speed="6"
+					data-scroll-delay={(0.04 + 0.01 * (index + 1)).toFixed(2)}
+				>
+					{letter}
 				</HeaderContent>
 			))}
 		</StyledHeader>
